8abb42095a95510838fc316ab8ec99253b0b43f0
Ben <ben@haynesdigital.com>
2016-02-05 17:52:26 -0500

    Changed stuff.
    Probably should document this.

--- src/DTT.c
+++ src/DTT.c
@@ -1,4 +1,6 @@
 #include <pebble.h>
+//Make some changes here
+//Benjamin Haynes 2016
 
 Window *window;
 TextLayer *text_layer;
@@ -7,18 +9,18 @@ void handle_init(void) {
 	// Create a window and text layer
 	window = window_create();
 	text_layer = text_layer_create(GRect(0, 0, 144, 154));
-	
+
 	// Set the text, font, and text alignment
-	text_layer_set_text(text_layer, "Hi, I'm a Pebble!");
+	text_layer_set_text(text_layer, "Daily Tasks go here.");
 	text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
 	text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-	
+
 	// Add the text layer to the window
 	layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer));
 
 	// Push the window
 	window_stack_push(window, true);
-	
+
 	// App Logging!
 	APP_LOG(APP_LOG_LEVEL_DEBUG, "Just pushed a window!");
 }
@@ -26,7 +28,7 @@ void handle_init(void) {
 void handle_deinit(void) {
 	// Destroy the text layer
 	text_layer_destroy(text_layer);
-	
+
 	// Destroy the window
 	window_destroy(window);
 }
